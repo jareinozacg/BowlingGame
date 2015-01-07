96a3cf41dcc5f7cee8d7c7bf7caa9da5017b34ee
Alfonso Reinoza <jareinozacg@gmail.com>
2015-01-07 08:45:55 -0430

    Commit score

--- BowlingGame/classGame.py
+++ BowlingGame/classGame.py
@@ -12,3 +12,18 @@ class Game():
     def roll(self, pins):
         self._rolls[self._currentRoll] = pins
         self._currentRoll += 1
+
+    def score(self):
+        score = 0
+        frameIndex  = 0
+        for frame in range(10):
+            if self._isStrike(frameIndex):
+                score += 10 + self._strikeBonus(frameIndex)
+                frameIndex  += 1
+            elif self._isSpare(frameIndex):
+                score += 10 + self._spareBonus(frameIndex)
+                frameIndex  += 2
+            else:
+                score += self._sumOfBallsInFrame(frameIndex)
+                frameIndex  += 2
+        return score
